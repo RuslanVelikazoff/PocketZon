93792cacfc62ac4dcc69655ab2d4e3597ce90eb4
RuslanVelikazoff <123361452+RuslanVelikazoff@users.noreply.github.com>
2024-03-27 11:50:19 +0300

    Create tilemap and player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction;
+
+    [SerializeField] 
+    private Joystick joystick;
+    
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+    
+    private void Update()
+    {
+        direction.x = joystick.Horizontal;
+        direction.y = joystick.Vertical;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * speed * Time.fixedDeltaTime);
+    }
+}
